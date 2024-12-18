49780471d9dcb6c040acd9d6163f04962cea9e65
von <137023645+YiTsenHo@users.noreply.github.com>
2024-12-19 00:16:29 +0800

    Delete sukiyaki/sukiyaki directory

--- sukiyaki/sukiyaki/script.js
+++
@@ -1,9 +0,0 @@
-function showContent(id) {
-    // 隱藏所有內容
-    const contents = document.querySelectorAll('.content');
-    contents.forEach(content => content.classList.remove('active'));
-
-    // 顯示所選內容
-    const selectedContent = document.getElementById(id);
-    selectedContent.classList.add('active');
-}
